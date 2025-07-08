b67f660aee571be4f6efbd89264bc2d49f8481ca
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-07-08 01:01:58 -0300

    Bump version to 0.1-alpha6

--- src/nouzen.h
+++ src/nouzen.h
@@ -3,7 +3,7 @@
 
 #define PROJECT_NAME "Nouzen"
 #define PROJECT_NAME_LOWERCASE "nouzen"
-#define PROJECT_VERSION "0.1-alpha5"
+#define PROJECT_VERSION "0.1-alpha6"
 #define PROJECT_REPOSITORY "https://github.com/AmanoTeam/Nouzen"
 
 #endif
