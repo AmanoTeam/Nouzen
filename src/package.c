9a997f4c540ace6cf0e31966cf90759ecbe6fb26
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-01-27 23:13:20 -0300

    Bump version

--- src/package.c
+++ src/package.c
@@ -558,15 +558,11 @@ int pkg_parse(
 	key = get_field_name(type, PKG_SECTION_FIELD_SIZE);
 	pkg->size = query_get_uint(query, key);
 	
-	if (pkg->size != 0 && type == REPO_TYPE_APK) {
-		pkg->size = pkg->size * 1000;
-	}
-	
 	/* Installed-Size */
 	key = get_field_name(type, PKG_SECTION_FIELD_INSTALLED_SIZE);
 	pkg->installed_size = query_get_uint(query, key);
 	
-	if (pkg->installed_size != 0) {
+	if (pkg->installed_size != 0 && type == REPO_TYPE_APT) {
 		pkg->installed_size = pkg->installed_size * 1000;
 	}
 	
